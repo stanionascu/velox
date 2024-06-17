be78ad2e6604c6775714a80302e2e05dea6c150e
Wei He <weihe@meta.com>
2024-06-17 11:41:51 -0700

    Extend expression fuzzer to use PQR
    Summary: This is a draft. Do not review now.
    
    Differential Revision: D58683623

--- velox/exec/fuzzer/FuzzerUtil.h
+++ velox/exec/fuzzer/FuzzerUtil.h
@@ -17,6 +17,8 @@
 
 #include "velox/core/PlanNode.h"
 #include "velox/exec/Split.h"
+#include "velox/exec/fuzzer/ReferenceQueryRunner.h"
+#include "velox/exec/tests/utils/QueryAssertions.h"
 
 namespace facebook::velox::exec::test {
 const std::string kHiveConnectorId = "test-hive";
@@ -81,4 +83,24 @@ bool containsUnsupportedTypes(const TypePtr& type);
 
 // Invoked to set up memory system with arbitration.
 void setupMemory(int64_t allocatorCapacity, int64_t arbitratorCapacity);
+
+enum ReferenceQueryErrorCode {
+  kSuccess,
+  kReferenceQueryFail,
+  kReferenceQueryUnsupported
+};
+
+std::pair<std::optional<MaterializedRowMultiset>, ReferenceQueryErrorCode>
+computeReferenceResults(
+    const core::PlanNodePtr& plan,
+    const std::vector<RowVectorPtr>& input,
+    ReferenceQueryRunner* referenceQueryRunner);
+
+// Will throw if referenceQueryRunner doesn't support
+// returning results as a vector.
+std::pair<std::optional<std::vector<RowVectorPtr>>, ReferenceQueryErrorCode>
+computeReferenceResultsAsVector(
+    const core::PlanNodePtr& plan,
+    const std::vector<RowVectorPtr>& input,
+    ReferenceQueryRunner* referenceQueryRunner);
 } // namespace facebook::velox::exec::test
